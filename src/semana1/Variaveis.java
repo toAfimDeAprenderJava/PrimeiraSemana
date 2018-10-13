82cf3b451387419dd57f10f56fa67fe55fab6e41
Tácito Lima <tacito.tp@gmail.com>
2018-10-13 20:11:59 -0300

    Teste para subir exercicio PrimeiraSemana

--- src/semana1/Variaveis.java
+++ src/semana1/Variaveis.java
@@ -20,5 +20,28 @@ public class Variaveis {
 
     public static void main(String[] args) {
 
+        int varInt = 15;
+        long varLong = 444444444;
+        double varDouble = 3.55;
+        char varChar = 'C';
+        boolean varBoolean = true;
+
+        int varIntDois = 31;
+        long varLongDois = 33131;
+        double varDoubleDois = 533.55;
+        char varCharDois = 'U';
+        boolean varBooleanDois = false;
+
+        System.out.println("Nome da variavel 'varInt' | Explicacao da Variavel: Variavel responsável por armazezar tipos inteiros ex: 1,2 3..etc | Tipo da variavel: int | conteudo da variavel "+ varInt);
+        System.out.println("Nome da variavel 'varLong' | Explicacao da Variavel: Variavel responsável por armazezar tipos inteiros com maior range que o 'int' | Tipo da variavel 'long' | conteudo da variavel " + varLong);
+        System.out.println("Nome da variavel 'varDouble' | Explicacao da Variavel: Variavel responsável por armazenar tipos de dados de ponto flutuante ex: 2.55  'double' | Tipo da variavel 'double' | conteudo da variavel "+ varDouble);
+        System.out.println("Nome da variavel 'varChar'  | Explicacao da Variavel: Variavel responsável por armazenar um caractere ex: A, B, C | Tipo da variavel 'char' | conteudo da variavel "+ varChar );
+        System.out.println("Nome da variavel 'varDouble'  | Explicacao da Variavel:  Variavel responsavel por armazenar tipo booleanos Verdadeiro (true) e Falso (false) | Tipo da variavel 'boolean' | Conteudo da variavel "+ varBoolean);
+
+        System.out.println("Nome da variavel 'varIntDois' | Explicacao da Variavel: Variavel responsável por armazezar tipos inteiros ex: 1,2 3..etc | Tipo da variavel: int | conteudo da variavel "+ varIntDois);
+        System.out.println("Nome da variavel 'varLongDois' | Explicacao da Variavel: Variavel responsável por armazezar tipos inteiros com maior range que o 'int' | Tipo da variavel 'long' | conteudo da variavel " + varLongDois);
+        System.out.println("Nome da variavel 'varDoubleDois' | Explicacao da Variavel: Variavel responsável por armazenar tipos de dados de ponto flutuante ex: 2.55  'double' | Tipo da variavel 'double' | conteudo da variavel "+ varDoubleDois);
+        System.out.println("Nome da variavel 'varCharDois'  | Explicacao da Variavel: Variavel responsável por armazenar um caractere ex: A, B, C | Tipo da variavel 'char' | conteudo da variavel "+ varCharDois );
+        System.out.println("Nome da variavel 'varDoubleDois'  | Explicacao da Variavel:  Variavel responsavel por armazenar tipo booleanos Verdadeiro (true) e Falso (false) | Tipo da variavel 'boolean' | Conteudo da variavel "+ varBooleanDois);
     }
 }
