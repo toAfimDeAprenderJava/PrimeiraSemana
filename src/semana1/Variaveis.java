671651135506a4c38e4b8e34fb2194f5ee134ca8
Freitas <664586@cognizant.com>
2018-10-15 10:13:10 -0300

    exercicio concluido

--- src/semana1/Variaveis.java
+++ src/semana1/Variaveis.java
@@ -1,7 +1,6 @@
 package semana1;
 
 /**
- *  Como fazer o pull reuest: https://blog.da2k.com.br/2015/02/04/git-e-github-do-clone-ao-pull-request/
  *  Descreva 5 tipos de variaveis  e associe 10 variavel com algum dado compativel com ela.
  *  
  *  Armazene a descrição da variavel em uma variavel e imprima as variaveis no console do seguinte modo:
